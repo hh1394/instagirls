0bae8aaad612746669c9ca6e03b2be69d67df66e
sombra-hlieb-halunko <massmurder667>
2021-05-15 13:10:24 +0300

    deleted old classes, added scheduling pt3

---
+++ src/main/java/com/instagirls/service/instagram/InstagramService.java
@@ -0,0 +1,203 @@
+package com.instagirls.service.instagram;
+
+import com.github.instagram4j.instagram4j.IGClient;
+import com.github.instagram4j.instagram4j.exceptions.IGLoginException;
+import com.github.instagram4j.instagram4j.models.media.timeline.TimelineMedia;
+import com.github.instagram4j.instagram4j.requests.feed.FeedUserRequest;
+import com.github.instagram4j.instagram4j.responses.feed.FeedUserResponse;
+import com.instagirls.dto.InstagramPostDTO;
+import com.instagirls.exception.LoginFailedException;
+import com.instagirls.exception.SleepFailedException;
+import com.instagirls.model.instagram.InstagramAccount;
+import com.instagirls.model.instagram.InstagramMedia;
+import com.instagirls.model.instagram.InstagramPost;
+import com.instagirls.repository.InstagramAccountRepository;
+import com.instagirls.repository.InstagramMediaRepository;
+import com.instagirls.repository.InstagramPostRepository;
+import com.instagirls.util.InstagramMediaExtractor;
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static com.instagirls.model.instagram.InstagramPost.DEFAULT_CAPTION;
+
+@Service
+public class InstagramService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InstagramService.class);
+    private static IGClient igClient;
+    private static int loginRetryCounter = 0;
+
+    @Autowired
+    private InstagramAccountRepository instagramAccountRepository;
+
+    @Autowired
+    private InstagramPostRepository instagramPostRepository;
+
+    @Autowired
+    private InstagramMediaRepository instagramMediaRepository;
+
+    @PostConstruct
+    private void login() {
+        if (igClient == null || !igClient.isLoggedIn()) {
+            LOGGER.info("IG Client not logged in!");
+            performLogin();
+        } else {
+            LOGGER.info("IG Client logged in!");
+        }
+    }
+
+    @NotNull
+    public InstagramPostDTO getNewMostLikedPostFromRandomAccount() {
+        final InstagramAccount instagramAccount = getRandomAccount();
+        Optional<InstagramPost> instagramPost = instagramAccount.getInstagramPosts().stream()
+                .sorted(Comparator.comparingInt(InstagramPost::getLikes))
+                .filter(ip -> !ip.isPosted())
+                .findFirst();
+        if (instagramPost.isPresent()) {
+            InstagramPost post = instagramPost.get();
+            LOGGER.info("New most like post ID: " + post.getId());
+            return InstagramPostDTO.builder()
+                    .instagramPost(post)
+                    .instagramAccountURL("https://www.instagram.com/" + instagramAccount.getUsername() + "/")
+                    .build();
+        } else {
+            disableAccount(instagramAccount);
+            return getNewMostLikedPostFromRandomAccount();
+        }
+    }
+
+    public void loadNewAccount(final String username) {
+        LOGGER.info("Loading new account: " + username);
+        InstagramAccount instagramAccount = new InstagramAccount(username);
+        instagramAccount = instagramAccountRepository.save(instagramAccount);
+        loadPostsForAccount(instagramAccount);
+    }
+
+
+    @SneakyThrows
+    // TODO load only new
+    private void loadPostsForAccount(final InstagramAccount instagramAccount) {
+        final Set<TimelineMedia> items = new HashSet<>();
+        FeedUserResponse feedUserResponse;
+
+        final Long pk = getUserPK(instagramAccount);
+        final FeedUserRequest request = new FeedUserRequest(pk);
+        feedUserResponse = igClient.sendRequest(request).get();
+        items.addAll(feedUserResponse.getItems());
+
+        while (feedUserResponse.isMore_available()) {
+            request.setMax_id(feedUserResponse.getNext_max_id());
+            feedUserResponse = igClient.sendRequest(request).get();
+            items.addAll(feedUserResponse.getItems());
+            LOGGER.info("Batch: " + feedUserResponse.getItems().size());
+            LOGGER.info("Overall: " + items.size());
+            LOGGER.info("Loading more posts!");
+        }
+        LOGGER.info("Done loading posts!");
+
+        List<InstagramPost> instagramPosts = items.stream().map(this::saveAsInstagramPostWithMedia).collect(Collectors.toList());
+        instagramAccount.setInstagramPosts(instagramPosts);
+        instagramAccountRepository.save(instagramAccount);
+    }
+
+    private InstagramPost saveAsInstagramPostWithMedia(final TimelineMedia timelineMedia) {
+
+        List<InstagramMedia> instagramMedia = InstagramMediaExtractor.extractMedia(timelineMedia);
+        instagramMediaRepository.saveAll(instagramMedia);
+
+        InstagramPost instagramPost = new InstagramPost();
+
+        instagramPost.setInstagramPostId(timelineMedia.getId());
+        instagramPost.setPosted(false);
+        instagramPost.setCaption(timelineMedia.getCaption() != null ? timelineMedia.getCaption().getText() : DEFAULT_CAPTION);
+        instagramPost.setLikes(timelineMedia.getLike_count());
+        instagramPost.setInstagramMedia(instagramMedia);
+        instagramPostRepository.save(instagramPost);
+
+        return instagramPost;
+    }
+
+    @SneakyThrows
+    private Long getUserPK(final InstagramAccount instagramAccount) {
+
+        if (instagramAccount.getInstagramPk() != null) {
+            return instagramAccount.getInstagramPk();
+        }
+
+        return loadUserPk(instagramAccount);
+    }
+
+    private Long loadUserPk(final InstagramAccount instagramAccount) throws InterruptedException, ExecutionException {
+        Long pk = igClient.actions().users()
+                .findByUsername(instagramAccount.getUsername()).get().getUser().getPk();
+        instagramAccount.setInstagramPk(pk);
+        instagramAccountRepository.save(instagramAccount);
+        return pk;
+    }
+
+    private void disableAccount(final InstagramAccount instagramAccount) {
+        LOGGER.info("No posts for " + instagramAccount.getUsername());
+        LOGGER.info("Switching off!");
+        instagramAccount.setActive(false);
+        instagramAccountRepository.save(instagramAccount);
+    }
+
+    private InstagramAccount getRandomAccount() {
+        final List<InstagramAccount> allAccounts = (List<InstagramAccount>) instagramAccountRepository.findAll();
+        InstagramAccount instagramAccount = allAccounts.stream().filter(InstagramAccount::isActive).findAny()
+                .orElseThrow(() -> new IllegalStateException("No instagram accounts in DB!"));
+        LOGGER.info("Random instagram account: " + instagramAccount.getUsername());
+        return instagramAccount;
+    }
+
+    private void performLogin() {
+        LOGGER.info("Performing login.. ");
+        try {
+            loginToInstagram();
+        } catch (final IGLoginException e) {
+            if (loginRetryCounter < 3) {
+                retryLogin();
+            } else {
+                throw new LoginFailedException("Login failed after 3 retries!", e);
+            }
+        }
+        LOGGER.info("Login performed.");
+    }
+
+    private void retryLogin() {
+        ++loginRetryCounter;
+        LOGGER.info("Login failed. Retrying in 1 minute..");
+        sleep(1);
+        performLogin();
+    }
+
+    private void loginToInstagram() throws IGLoginException {
+        igClient = IGClient.builder()
+                .username(System.getenv("instagram_username"))
+                .password(System.getenv("instagram_password"))
+                .login();
+    }
+
+    private void sleep(final int minutes) {
+        try {
+            Thread.sleep(TimeUnit.MINUTES.toMillis(minutes));
+        } catch (final InterruptedException interruptedException) {
+            throw new SleepFailedException("Login sleep failed.", interruptedException);
+        }
+    }
+
+    public void setPosted(final InstagramPost instagramPost) {
+        instagramPost.setPosted(true);
+        instagramPostRepository.save(instagramPost);
+    }
+}
---
+++ src/main/java/com/instagirls/service/telegram/TelegramService.java
@@ -0,0 +1,181 @@
+package com.instagirls.service.telegram;
+
+import com.instagirls.dto.InstagramPostDTO;
+import com.instagirls.model.instagram.InstagramMedia;
+import com.instagirls.model.telegram.TelegramPost;
+import com.instagirls.model.telegram.TelegramVote;
+import com.instagirls.repository.TelegramPostRepository;
+import com.instagirls.repository.TelegramVoteRepository;
+import com.instagirls.service.instagram.InstagramService;
+import com.instagirls.util.PostMapper;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.model.request.InputMedia;
+import com.pengrad.telegrambot.model.request.InputMediaPhoto;
+import com.pengrad.telegrambot.request.EditMessageReplyMarkup;
+import com.pengrad.telegrambot.request.SendMediaGroup;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TelegramService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TelegramService.class);
+    private static final String UPDATE_MESSAGE = "send_new_girl";
+    private static final String CHAT_ID = System.getenv("chat_id");
+    private TelegramBot bot;
+
+    @Autowired
+    private TelegramVoteRepository telegramVoteRepository;
+
+    @Autowired
+    private TelegramPostRepository telegramPostRepository;
+
+    @Autowired
+    private InstagramService instagramService;
+
+    @PostConstruct
+    private void initBot() {
+        LOGGER.info("Initializing bot..");
+        if (bot == null) {
+            bot = new TelegramBot(System.getenv("bot_token"));
+            bot.setUpdatesListener(this::processUpdates);
+            LOGGER.info("Created new bot.");
+        } else {
+            LOGGER.info("Bot already initialized.");
+        }
+    }
+
+    @Scheduled(cron = "0 8 * * *")
+    public void sendNewPostToTelegram() {
+        initBot();
+        final InstagramPostDTO instagramPostDTO = instagramService.getNewMostLikedPostFromRandomAccount();
+        final TelegramPost telegramPost = new TelegramPost(instagramPostDTO.getInstagramPost());
+        sendContentToChat(telegramPost, instagramPostDTO.getInstagramAccountURL());
+
+        telegramPostRepository.save(telegramPost);
+        instagramService.setPosted(telegramPost.getInstagramPost());
+    }
+
+    private int processUpdates(final List<Update> updates) {
+        LOGGER.info(String.format("Got %s updates!", updates.size()));
+        updates.forEach(this::processUpdate);
+        return updates.get(updates.size() - 1).updateId();
+    }
+
+    private void processUpdate(final Update update) {
+        if (updateContainsValidVote(update)) {
+            final TelegramVote telegramVote = PostMapper.mapToTelegramVote(update);
+            telegramVoteRepository.save(telegramVote);
+            LOGGER.info("Got request for a new girl!");
+            LOGGER.info("User: " + update.callbackQuery().from());
+            checkVotes();
+        }
+    }
+
+    private void checkVotes() {
+        if (isEnoughVotes()) {
+            sendNewPostToTelegram();
+        }
+    }
+
+    private void sendContentToChat(final TelegramPost telegramPost, final String instagramAccountURL) {
+        sendMedia(telegramPost);
+        sendCaptionWithReplyKeyboardMarkup(telegramPost, instagramAccountURL);
+    }
+
+    private void sendCaptionWithReplyKeyboardMarkup(final TelegramPost telegramPost, final String instagramAccountURL) {
+        final SendResponse response = sendCaption(telegramPost);
+        addReplyKeyboardMarkup(response, instagramAccountURL);
+    }
+
+    private void addReplyKeyboardMarkup(final SendResponse response, final String instagramAccountURL) {
+        final InlineKeyboardMarkup replyKeyboardMarkup = getReplyInlineKeyboardMarkup(instagramAccountURL);
+        sendReplyKeyboardMarkup(response, replyKeyboardMarkup);
+    }
+
+    private void sendReplyKeyboardMarkup(final SendResponse response, final InlineKeyboardMarkup replyKeyboardMarkup) {
+        final Integer messageId = response.message().messageId();
+        final EditMessageReplyMarkup editMessageReplyMarkup = new EditMessageReplyMarkup(CHAT_ID, messageId);
+        editMessageReplyMarkup.replyMarkup(replyKeyboardMarkup);
+        bot.execute(editMessageReplyMarkup);
+    }
+
+    @NotNull
+    private InlineKeyboardMarkup getReplyInlineKeyboardMarkup(final String instagramAccountURL) {
+        final InlineKeyboardButton girlAccountUrlKeyboardButton = buildInstagramAccountLinkKeyboardButton(instagramAccountURL);
+        final InlineKeyboardButton voteKeyboardButton = buildVoteKeyboardButton();
+        return buildKeyboardMarkup(girlAccountUrlKeyboardButton, voteKeyboardButton);
+    }
+
+    @NotNull
+    private InlineKeyboardMarkup buildKeyboardMarkup(final InlineKeyboardButton girlAccountUrlKeyboardButton, final InlineKeyboardButton voteKeyboardButton) {
+        final InlineKeyboardMarkup replyKeyboardMarkup = new InlineKeyboardMarkup();
+        replyKeyboardMarkup.addRow(girlAccountUrlKeyboardButton);
+        replyKeyboardMarkup.addRow(voteKeyboardButton);
+        return replyKeyboardMarkup;
+    }
+
+    @NotNull
+    private InlineKeyboardButton buildVoteKeyboardButton() {
+        final int newTelegramPostCounter = telegramVoteRepository.findByTelegramPost(telegramPostRepository.findTopByOrderByIdDesc()).size();
+        final InlineKeyboardButton voteKeyboardButton = new InlineKeyboardButton(String.format("Send New Girl! (%s\\4)", newTelegramPostCounter));
+        voteKeyboardButton.callbackData(UPDATE_MESSAGE);
+        return voteKeyboardButton;
+    }
+
+    @NotNull
+    private InlineKeyboardButton buildInstagramAccountLinkKeyboardButton(final String instagramAccountURL) {
+        final InlineKeyboardButton girlAccountUrlKeyboardButton = new InlineKeyboardButton("Girl Instagram");
+        girlAccountUrlKeyboardButton.url(instagramAccountURL);
+        return girlAccountUrlKeyboardButton;
+    }
+
+    private SendResponse sendCaption(final TelegramPost telegramPost) {
+        final SendMessage sendMessage = new SendMessage(CHAT_ID, telegramPost.getInstagramPost().getCaption());
+        return bot.execute(sendMessage);
+    }
+
+    private void sendMedia(final TelegramPost telegramPost) {
+        final List<InputMedia<?>> medias = mapMedias(telegramPost);
+        final SendMediaGroup request = new SendMediaGroup(CHAT_ID, medias.toArray(new InputMedia[0]));
+        bot.execute(request);
+    }
+
+    private List<InputMedia<?>> mapMedias(final TelegramPost telegramPost) {
+        final List<InputMedia<?>> inputMedia = new ArrayList<>();
+        for (InstagramMedia instagramPostMedia : telegramPost.getInstagramPost().getInstagramMedia()) {
+            InputMediaPhoto photo = new InputMediaPhoto(instagramPostMedia.getUrl());
+            inputMedia.add(photo);
+        }
+        return inputMedia;
+    }
+
+
+    private boolean isEnoughVotes() {
+        final TelegramPost currentPost = telegramPostRepository.findTopByOrderByIdDesc();
+        List<TelegramVote> votes = telegramVoteRepository.findByTelegramPost(currentPost);
+        return votes.size() > 3;
+    }
+
+    private boolean updateContainsValidVote(final Update update) {
+        final boolean isUpdateGirl = update.callbackQuery().data().equals(UPDATE_MESSAGE);
+        final boolean isNewUserVote =
+                telegramVoteRepository.findByTelegramUserIdAndTelegramPost(update.callbackQuery().from().id(),
+                        telegramPostRepository.findTopByOrderByIdDesc());
+        return isUpdateGirl && isNewUserVote;
+    }
+
+}
