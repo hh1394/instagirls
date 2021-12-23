48b45ebc6026582975e62f7f3e009aa26aa72c78
HH1394 <hhalunko@gmail.com>
2021-12-23 21:17:44 +0200

    common module

---
+++ telegram-bot/src/main/java/com/instagirls/service/InstagramService.java
@@ -0,0 +1,13 @@
+package com.instagirls.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstagramService {
+    public InstagramPostDTO getNewMostLikedPostFromAccount(final String account) {
+
+    }
+
+    public InstagramPostDTO getNewMostLikedPostFromRandomAccount() {
+    }
+}
